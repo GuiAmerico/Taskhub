328e65b2687520632a7b56c9ce1166c0bfacc19f
Guilherme Américo <107080927+GuiAmerico@users.noreply.github.com>
2023-06-03 14:25:01 -0300

    :tada: init: inital commit

---
+++ src/test/java/com/americo/taskhub/TaskhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.americo.taskhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
